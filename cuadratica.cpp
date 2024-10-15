45cfe7a3645826c674c85912795f87d6e7ec9f39
Fenx224 <zagj080422hgtvrsa8@cbtis217.edu.mx>
2024-10-15 07:56:29 -0600

    Creamos el programa

---
+++ cuadratica.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main() {
+	double a, b, c;
+	cout<<"introduce los valores de A, B y C de la ecuacion que desees solucionar"<<endl;
+	cout<<"A:"<<endl;
+	cin>>a;
+	cout<<"B:"<<endl;
+	cin>>b;
+	cout<<"C:"<<endl;
+	cin>>c;
+	cout<<"El resultado es:"<<endl;
+	cout<<"x1: "<<setprecision(3)<<((b*-1)+sqrt((b*b)-((a*c)*4)))/(a*2)<<endl;
+	cout<<"x2: "<<setprecision(3)<<((b*-1)-sqrt((b*b)-((a*c)*4)))/(a*2)<<endl;
+}
