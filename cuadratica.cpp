6aad7faa03409ea56090e79ae6e928d46940e8f2
Fenx224 <zagj080422hgtvrsa8@cbtis217.edu.mx>
2024-10-15 08:25:41 -0600

    Eliminamos los comentarios previamente puesto y dejamos unicamente uno explicando que hace el programa

--- cuadratica.cpp
+++ cuadratica.cpp
@@ -1,24 +1,19 @@
-//Colocamos las librerias//
+//Este es un programa basico en c++ que utilizaremos para resolver ecuaciones cuadraticas por//
+//medio de la formula general//
 #include <bits/stdc++.h>
 #include <math.h>
 using namespace std;
 
-//iniciamos el programa//
 int main() {
-	//Definimos las variables que utilizaremos//
 	double a, b, c;
-	//Le pedimos al usuario que introduzca los valores//
-	cout<<"introduce los valores de A, B y C de la ecuacion que desees solucionar"<<endl;
-	//Guardamos los valores en la variable correspondiente//
-	cout<<"A:"<<endl;
-	cin>>a;
-	cout<<"B:"<<endl;
-	cin>>b;
-	cout<<"C:"<<endl;
-	cin>>c;
-	//Mostramos el resultado//
-	cout<<"El resultado es:"<<endl;
-	//Realizamos las operaciones e imprimimos los resultado redondeandolo a 2 decimales//
-	cout<<"x1: "<<setprecision(3)<<((b*-1)+sqrt((b*b)-((a*c)*4)))/(a*2)<<endl;
-	cout<<"x2: "<<setprecision(3)<<((b*-1)-sqrt((b*b)-((a*c)*4)))/(a*2)<<endl;
+	cout << "introduce los valores de A, B y C de la ecuacion que desees solucionar" << endl;
+	cout << "A:" << endl;
+	cin >> a;
+	cout << "B:" << endl;
+	cin >> b;
+	cout << "C:" << endl;
+	cin >> c;
+	cout << "El resultado es:" << endl;
+	cout << "x1: "<< setprecision(3) << ((b*-1)+sqrt((b*b)-((a*c)*4)))/(a*2) << endl;
+	cout << "x2: "<< setprecision(3) << ((b*-1)-sqrt((b*b)-((a*c)*4)))/(a*2) << endl;
 }
