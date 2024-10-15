3f8fb1adbcafa63d7b3a8c4d63763f1e9c1191b0
Fenx224 <zagj080422hgtvrsa8@cbtis217.edu.mx>
2024-10-15 08:08:08 -0600

    Agregamos comentarios diciendo que hace cada parte del codigo

--- cuadratica.cpp
+++ cuadratica.cpp
@@ -1,17 +1,24 @@
+//Colocamos las librerias//
 #include <bits/stdc++.h>
 #include <math.h>
 using namespace std;
 
+//iniciamos el programa//
 int main() {
+	//Definimos las variables que utilizaremos//
 	double a, b, c;
+	//Le pedimos al usuario que introduzca los valores//
 	cout<<"introduce los valores de A, B y C de la ecuacion que desees solucionar"<<endl;
+	//Guardamos los valores en la variable correspondiente//
 	cout<<"A:"<<endl;
 	cin>>a;
 	cout<<"B:"<<endl;
 	cin>>b;
 	cout<<"C:"<<endl;
 	cin>>c;
+	//Mostramos el resultado//
 	cout<<"El resultado es:"<<endl;
+	//Realizamos las operaciones e imprimimos los resultado redondeandolo a 2 decimales//
 	cout<<"x1: "<<setprecision(3)<<((b*-1)+sqrt((b*b)-((a*c)*4)))/(a*2)<<endl;
 	cout<<"x2: "<<setprecision(3)<<((b*-1)-sqrt((b*b)-((a*c)*4)))/(a*2)<<endl;
 }
